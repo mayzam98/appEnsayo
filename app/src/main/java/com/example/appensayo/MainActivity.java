6ace5f07c02c7c4fbe4d0a0cd419230b1601ff99
mayzam98 <83322310+mayzam98@users.noreply.github.com>
2021-11-16 18:34:05 -0500

    Creacion de actividad de login y de styles.xml

--- app/src/main/java/com/example/appensayo/MainActivity.java
+++ app/src/main/java/com/example/appensayo/MainActivity.java
@@ -40,29 +40,27 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         });*/
 
 
-        //Metodo3:Poner una ID al controlado..., crear una variable del tipo de controlador... en el onCreate debemos encontrar la vista con el ID que le dimos
 
-        btn_perfil = findViewById(R.id.btn_perfil);//Los botones tienen un ID numerico y co
-        btn_ajustes = findViewById(R.id.btn_ajustes);
-        btn_acerca_de = findViewById(R.id.btn_acerca_de);
 
-        //Asignamos un listener pero no se lo voy a crear a cad uno, le voy a poner THIS... para que sea solo un metodo que se encargue de darle soporte a todos
-        //Mi actividad se va a encargar de implementar el metodo y se va a encargar de hacer las importaciones
-        //Le doy click en this y utilizo la ayuda del android studio.... make Implementar
-        btn_jugar.setOnClickListener(this);
-        btn_perfil.setOnClickListener(this);
-        btn_ajustes.setOnClickListener(this);
-        btn_acerca_de.setOnClickListener(this);
+
+            //Metodo3:Poner una ID al controlado..., crear una variable del tipo de controlador... en el onCreate debemos encontrar la vista con el ID que le dimos
+
+            btn_perfil = findViewById(R.id.btn_perfil);//Los botones tienen un ID numerico y co
+            btn_ajustes = findViewById(R.id.btn_ajustes);
+            btn_acerca_de = findViewById(R.id.btn_acerca_de);
+
+            //Asignamos un listener pero no se lo voy a crear a cad uno, le voy a poner THIS... para que sea solo un metodo que se encargue de darle soporte a todos
+            //Mi actividad se va a encargar de implementar el metodo y se va a encargar de hacer las importaciones
+            //Le doy click en this y utilizo la ayuda del android studio.... make Implementar
+            btn_jugar.setOnClickListener(this);
+            btn_perfil.setOnClickListener(this);
+            btn_ajustes.setOnClickListener(this);
+            btn_acerca_de.setOnClickListener(this);
 
     }
 
 
-    //Metodo 1: en el xml defino una propiedad android:onClick="clickJugar" y le envio el metodo que me llama cuando le doy click
-   /* public void clickJugar(View v){ //estos listener siempre reciben un View
-        Toast.makeText(this, "Le dist a JUGAR perro!", Toast.LENGTH_SHORT).show();
-        Log.e("CLICK_JUGAR","LE HE DADO AL BOTON JUGAR");
-    }*/
-    //Los listener siempre arrancan con la palabra on..un listener que esta esperando la interaccion del usuario
+
 
 
     //Metodo3: implementacion del metodo onClick de manera GLOBAL. Aca en el View es el que me va a servir para identificar que boton se ha presionado, apartir del ID
