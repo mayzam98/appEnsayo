004aefee9c263289aa4b974be357fe1d212aaa64
mayzam98 <83322310+mayzam98@users.noreply.github.com>
2021-11-16 23:25:04 -0500

    Creacion de Menu Drawer,

--- app/src/main/java/com/example/appensayo/LoginActivity.java
+++ app/src/main/java/com/example/appensayo/LoginActivity.java
@@ -5,8 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
@@ -15,7 +17,8 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
 
     private Button btn_login;
     private Button btn_registro;
-
+    private EditText edt_usuario;
+    private EditText edt_contrasena;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +29,8 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
 
         btn_login = findViewById(R.id.btn_login);
         btn_registro = findViewById(R.id.btn_registro);
+        edt_usuario = findViewById(R.id.edt_usuario);
+        edt_contrasena = findViewById(R.id.edt_contrasena);
 
         btn_registro.setOnClickListener(this);
         btn_login.setOnClickListener(this);
@@ -35,6 +40,23 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.btn_login:
+
+                String usuario = edt_usuario.getText().toString();
+                String contrasena = edt_contrasena.getText().toString();
+
+                Log.e("USUARIO", usuario);
+                Log.e("CONTRASEÑA", contrasena);
+
+                if(usuario.equals("admin@admin.com") && contrasena.equals("admin")  ){
+                    Toast.makeText(LoginActivity.this, "Se ha iniciado sesion", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(LoginActivity.this, MenuActivity.class);
+                    startActivity(intent);
+
+                }else{
+                    Toast.makeText(LoginActivity.this, "Error iniciando sesion", Toast.LENGTH_SHORT).show();
+
+                }
+
                 break;
 
             case R.id.btn_registro:
--- app/src/main/java/com/example/appensayo/RegistroActivity.java
+++ app/src/main/java/com/example/appensayo/RegistroActivity.java
@@ -3,12 +3,75 @@ package com.example.appensayo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class RegistroActivity extends AppCompatActivity {
 
+    private CheckBox chb_terminos;
+    private Button btn_registro_usuario;
+    private EditText edt_contrasena;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
+
+        btn_registro_usuario = findViewById(R.id.btn_registro_usuario);
+        chb_terminos = findViewById(R.id.chb_terminos);
+        edt_contrasena = findViewById(R.id.edt_contrasena);
+
+
+        btn_registro_usuario.setEnabled(false);
+
+        /*chb_terminos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(chb_terminos.isChecked()){
+                    btn_registro_usuario.setEnabled(true);
+                }else{
+                    btn_registro_usuario.setEnabled(false);
+                }
+            }
+        });*/
+
+        chb_terminos.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                btn_registro_usuario.setEnabled(isChecked);
+
+            }
+        });
+
+        btn_registro_usuario.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String contrasena = edt_contrasena.getText().toString();
+
+                if(contrasena.length()<8 &&!isValidPassword(contrasena)){
+                    Toast.makeText(RegistroActivity.this, "Contraseña No valida", Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(RegistroActivity.this, "Contraseña valida", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    public static boolean isValidPassword(final String password) {
+
+        Pattern pattern;
+        Matcher matcher;
+        final String PASSWORD_PATTERN = "^(?=.*[0-9])(?=.*[A-Z])(?=.*[@#$%^&+=!])(?=\\S+$).{4,}$";
+        pattern = Pattern.compile(PASSWORD_PATTERN);
+        matcher = pattern.matcher(password);
+
+        return matcher.matches();
+
     }
 }
