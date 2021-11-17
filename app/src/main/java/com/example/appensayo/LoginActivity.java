8cf0934fd59a6fc9c14d0ce48ed456362c41d670
mayzam98 <83322310+mayzam98@users.noreply.github.com>
2021-11-16 19:27:51 -0500

    Creacion de actividad de registro

--- app/src/main/java/com/example/appensayo/LoginActivity.java
+++ app/src/main/java/com/example/appensayo/LoginActivity.java
@@ -2,13 +2,41 @@ package com.example.appensayo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button btn_login;
+    private Button btn_registro;
+    public Activity miActividad;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        miActividad = this;
+        btn_login = findViewById(R.id.btn_login);
+        btn_registro = findViewById(R.id.btn_registro);
+
+        btn_registro.setOnClickListener(this);
+        btn_login.setOnClickListener(this);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_login:
+                break;
+
+            case R.id.btn_registro:
+                Intent intent = new Intent(LoginActivity.this, RegistroActivity.class);
+                startActivity(intent);
+                break;
+        }
     }
 }
